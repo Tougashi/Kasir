17d0dcb2ca85bf84c696d86a11f441a5af03dc6c
Tougashiro <muhammadadryan505@gmail.com>
2024-02-06 10:11:54 +0700

    ui update

--- resources/views/Components/navbar.blade.php
+++ resources/views/Components/navbar.blade.php
@@ -1,29 +1,23 @@
-<nav class="navbar navbar-dark navbar-expand-lg w-80p float-end bg-success bg-opacity-75">
+<nav class="navbar navbar-dark navbar-expand-lg w-80p float-end bg-white">
     <div class="container">
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler bg-primary text-white" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    <a class="nav-link active text-primary" aria-current="page" href="#">Home</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="#">Link</a>
+                    <a class="nav-link text-primary" href="#">Link</a>
                 </li>
                 <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
-                        aria-expanded="false">
-                        Dropdown
-                    </a>
+                    <a class="nav-link dropdown-toggle text-primary" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Dropdown</a>
                     <ul class="dropdown-menu">
-                        <li><a class="dropdown-item" href="#">Action</a></li>
-                        <li><a class="dropdown-item" href="#">Another action</a></li>
-                        <li>
-                            <hr class="dropdown-divider">
-                        </li>
-                        <li><a class="dropdown-item" href="#">Something else here</a></li>
+                        <li><a class="dropdown-item text-primary" href="#">Action</a></li>
+                        <li><a class="dropdown-item text-primary" href="#">Another action</a></li>
+                        <li><hr class="dropdown-divider"></li>
+                        <li><a class="dropdown-item text-primary" href="#">Something else here</a></li>
                     </ul>
                 </li>
                 <li class="nav-item">
@@ -31,8 +25,8 @@
                 </li>
             </ul>
             <form class="d-flex" role="search">
-                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                <button class="btn btn-outline-success" type="submit">Search</button>
+                <input class="form-control me-2 border-primary border-solid" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-light btn-primary" type="submit">Search</button>
             </form>
         </div>
     </div>
--- resources/views/Components/sidebar.blade.php
+++ resources/views/Components/sidebar.blade.php
@@ -1,17 +1,20 @@
-<div class="container w-20p vh-100 bg-success bg-opacity-75 position-absolute shadow" id="sidebar">
-    <div class="px-2 mt-4 pb-5">
-        <img src="" alt="logo" class="img-fluid">
+<div class="container w-20p vh-100 bg-white position-absolute shadow" id="sidebar" style="border-right: 2px solid #007bff;">
+    <div class="px-2 mt-4 pb-4">
+        <img src="/assets/images/logo-text2.png" alt="logo" class="img-fluid mt-2">
     </div>
-
+    <hr class="text-primary">
+    <h5 class="sidebar-title text-white text-center mb-3 menu bg-primary rounded">MENU
+    </h5>
+    
+    <hr class="text-primary">
     <div class="sidebar-menu mt-4">
-        <p class="sidebar-title text-light mb-3 h6">Menu</p>
-        <a href="#" id="sidebarButton" class="btn w-100"><i class="bi bi-house-door-fill"></i> Dashboard</a>
-
+        <a href="#" id="sidebarButton" class="btn w-100 text-primary"><i class="bx bx-home-alt"></i> Dashboard</a>
+    
         <div class="accordion" onclick="toggleOverflowBar()">
             <div class="accordion-item">
               <h2 class="accordion-header">
                 <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="" aria-controls="collapseOne">
-                  Accordion Item #1
+                  <div class="text"><i class=""></i>Accordion Item #1</div>
                 </button>
               </h2>
               <div id="collapseOne" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
@@ -21,10 +24,11 @@
               </div>
             </div>
         </div>
-
-        <a href="#" id="sidebarButton" class="btn w-100"><i class="bi bi-folder2-open"></i> Data Barang</a>
-        <a href="#" id="sidebarButton" class="btn w-100"><i class="bi bi-people-fill"></i> Karyawan</a>
+    
+        <a href="#" id="sidebarButton" class="btn w-100 text-primary"><i class="bi bi-folder2-open"></i>Data Barang</a>
+        <a href="#" id="sidebarButton" class="btn w-100 text-primary"><i class="bi bi-people-fill"></i> Karyawan</a>
     </div>
+    
 
 </div>
 
@@ -33,9 +37,9 @@
         const sidebarButtons = $('a#sidebarButton');
         sidebarButtons.each(function(){
             $(this).on('mouseenter', function(){
-                $(this).addClass('btn-success');
+                $(this).addClass('btn-primary text-white');
             }).on('mouseleave', function(){
-                $(this).removeClass('btn-success');
+                $(this).removeClass('btn-primary text-white');
             });
         });
 
@@ -44,11 +48,9 @@
             if(isOverflow == 0){
                 isOverflow = 1;
                 $('#sidebar').addClass('overflow-scroll');
-                // alert('s');
             }else if(isOverflow == 1){
                 isOverflow = 0;
                 $('#sidebar').removeClass('overflow-scroll');
-                // alert('t');
             }
         }
 
