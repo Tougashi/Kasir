c1af100d2ac97d264daba1a8ea3c753be6dcd8a9
Tougashiro <muhammadadryan505@gmail.com>
2024-02-06 08:45:48 +0700

    form

--- resources/views/Auth/index.blade.php
+++ resources/views/Auth/index.blade.php
@@ -1,16 +1,11 @@
  <!DOCTYPE html>
  <html lang="en">
-
  <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{ $title }}</title>
-	<link href="{{ asset('/assets/plugins/perfect-scrollbar/css/perfect-scrollbar.css') }}" rel="stylesheet" />
-	<link href="{{ asset('assets/plugins/select2/css/select2.min.css') }}" rel="stylesheet" />
-	<link href="{{ asset('assets/plugins/select2/css/select2-bootstrap4.css') }}" rel="stylesheet" />
 	<link href="{{ asset('/assets/css/bootstrap.min.css') }}" rel="stylesheet">
-	<link href="{{ url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=swap') }}" rel="stylesheet">
 	<link href="{{ asset('/assets/css/app.css') }}" rel="stylesheet">
 	<link href="{{ asset('/assets/css/icons.css') }}" rel="stylesheet">
     <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
@@ -18,27 +13,23 @@
 	<script src="{{ asset('/assets/js/bootstrap.bundle.min.js') }}"></script>
 	<!--plugins-->
 	<script src="{{ asset('/assets/js/jquery.min.js') }}"></script>
-	<script src="{{ asset('/assets/plugins/simplebar/js/simplebar.min.js') }}"></script>
-	<script src="{{ asset('/assets/plugins/metismenu/js/metisMenu.min.js') }}"></script>
 	<script src="{{ asset('/assets/plugins/perfect-scrollbar/js/perfect-scrollbar.js') }}"></script>
-	<script src="{{ asset('/assets/plugins/vectormap/jquery-jvectormap-2.0.2.min.js') }}"></script>
-    <script src="{{ asset('/assets/plugins/vectormap/jquery-jvectormap-world-mill-en.js') }}"></script>
-	<script src="{{ asset('/assets/plugins/sparkline-charts/jquery.sparkline.min.js') }}"></script>
 </head>
 <body style="overflow: hidden">
         <div class="wrapper">
-            <div class="section-authentication-signin d-flex justify-content-center align-items-center my-5 ">
+            <div class="d-flex justify-content-center align-items-center ">
                 <div class="container">
+                    <br><br><br><br>
                     <div class="row row-cols-1 row-cols-lg-2 row-cols-xl-3">
-                        <div class="col mx-auto">
-                            <div class="card mt-auto rounded border-2 border-success">
+                        <div class="col-20 mx-auto">
+                            <div class="card mt-auto border-primary my-5 border-2 rounded">
                                 <div class="card-body">
                                     <div class="p-4 rounded">
                                         <div class="text-center">
-                                            <img src="{{asset('assets/images/logo-text.png')}}" alt="Kasir" width="300" class="rounded mx-auto d-block">
+                                            <img src="/assets/images/logo-text2.png" alt="Kasir" width="300" class="rounded mx-auto d-block">
                                         </div>
                                         <br>
-                                        <div class="form-body">
+                                        <div class="form-body mt-4">
                                             @if (Session::has('success'))
                                                 <div class="alert alert-success" role="alert">
                                                     {{ Session::get('success') }}
@@ -48,20 +39,20 @@
                                                 @csrf
                                                 <div class="col-12">
                                                     <label for="Username" class="form-label">Nama Pengguna</label>
-                                                    <input type="text" class="form-control border border-dark" id="username" placeholder="Masukan Nama Pengguna" name="username">
+                                                    <input type="text" class="form-control  border border-primary" id="username" placeholder="Masukan Nama Pengguna" required name="username">
                                                 </div>
                                                 <div class="col-12">
                                                     <label for="inputChoosePassword" class="form-label">Kata Sandi</label>
                                                     <div class="input-group" id="showHide">
-                                                        <input type="password" class="form-control border border-dark" id="password" placeholder="Masukan Kata Sandi" name="password">
-                                                        <a href="#" class="input-group-text bg-transparent border border-dark"><i class='bx bx-hide'></i></a>
+                                                        <input type="password" class="form-control  border border-primary" id="password" placeholder="Masukan Kata Sandi" required name="password">
+                                                        <a href="#" class="input-group-text bg-transparent border border-primary"><i class='bx bx-hide'></i></a>
                                                     </div>
                                                 </div>
-                                                <div class="col-md-6"><a href="#" class="text-success">Lupa Kata Sandi?</a></div>
+                                                <div class="col-md-6"><a href="#">Lupa Kata Sandi?</a></div>
                                                 {{-- <div class="col-md-6 text-end"><a href="/registrasi" class="text-black">Belum Punya Akun?</a></div> --}}
                                                 <div class="col-12">
                                                     <div class="d-grid">
-                                                        <button type="submit" class="btn btn-success"><i class="bx bxs-lock-open"></i>Masuk</button>
+                                                        <button type="submit" class="btn text-white bg-primary"><i class="bx bxs-lock-open"></i>Masuk</button>
                                                     </div>
                                                 </div>
                                             </form>
@@ -74,7 +65,7 @@
                 </div>
             </div>
         </div>
-    <footer class="bg-success shadow-sm border-top border-2 p-2 text-center fixed-bottom">
+        <footer class="bg-primary shadow-sm border-top border-2 p-2 text-center fixed-bottom">
         <p class="mb-0 footer text-light">© KOPERASI SISWA SMKN 2 TASIKMALAYA</p>
     </footer>
     {{-- HIDE & SHOW PASSWORD --}}
@@ -94,7 +85,6 @@
 			});
 		});
 	</script>
-    <script src="{{ asset('assets/js/particles.js') }}"></script>
     <script src="{{ asset('assets/js/script.js') }}"></script>
  </body>
 
