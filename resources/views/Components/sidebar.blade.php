f7b3d0b9f334144ddbf6e49ebff4b18386be9219
Ridho A <ridhoawwaludin@gmail.com>
2024-02-07 21:49:44 +0700

    Update sidebar

--- resources/views/Components/sidebar.blade.php
+++ resources/views/Components/sidebar.blade.php
@@ -1,4 +1,9 @@
-<div class="container w-20p bg-white position-absolute shadow" id="sidebar" style="border-right: 2px solid #007bff;">
+{{-- <div class="container w-20p bg-white position-absolute shadow" id="sidebar" style="border-right: 2px solid #007bff;">
+    <div class="position-absolute bg-body border border-start-0 my-2 rounded-3 rounded-start-0" style="left: 215px; z-index: 100;">
+        <div class="" onclick="toggleSidebar()" id="sidebarToggler">
+            <i class="bi bi-list h2"></i>
+        </div>
+    </div>
     <div class="px-2 mt-4 pb-4">
         <img src="/assets/images/logo-text2.png" alt="logo" class="img-fluid mt-2">
     </div>
@@ -15,8 +20,8 @@
           </li>
           <li class="">
             <ul id="menu" class="metismenu d-block">
-                <li class="mm-active">
-                  <a class="has-arrow" href="#" aria-expanded="true">Data Produk</a>
+                <li class="">
+                  <a class="has-arrow" href="#" aria-expanded="false">Data Produk</a>
                   <ul>
                     <li class="mm-show">
                         <a href="/admin/products/list" class="">
@@ -28,34 +33,81 @@
                 </li>
             </ul>
           </li>
-          {{-- <li>
-            <a href="/admin/products/list" class="">
-              <div class="parent-icon"><i class="bi bi-list-columns"></i></div>
-              <div class="menu-title">List Produk</div>
-            </a>
-          </li> --}}
         </ul>
       </div>
+</div> --}}
+
+<div class="container vh-100 bg-white position-absolute shadow" id="sidebarIconOnly"
+    style="">
+    {{-- <div class="position-absolute bg-body border border-start-0 my-2 rounded-3 rounded-start-0" style="left: 75px; z-index: 100;">
+        <div class="" onclick="toggleSidebar()" id="sidebarToggler">
+            <i class="bi bi-list h2"></i>
+        </div>
+    </div> --}}
+    <div class="mt-3 pb-5">
+        <img src="/assets/images/logo-text2.png" alt="logo" class="img-fluid mt-2">
+    </div>
+    <div class="sidebar-menu d-flex justify-content-center mt-5">
+        <ul class="text-center" id="menu">
+            <li>
+                <a href="/admin/dashboard" class="">
+                    <div class="parent-icon p-1"><i class='bi bi-house-door me-0 h5'></i></div>
+                </a>
+            </li>
+            <li class="">
+                <a href="/admin/dashboard" class="">
+                    <div class="parent-icon p-1"><i class="bi bi-list-columns me-0 h5"></i></div>
+                </a>
+            </li>
+            <li class="">
+                <a href="/admin/dashboard" class="">
+                    <div class="parent-icon p-1"><i class="bi bi-list-columns me-0 h5"></i></div>
+                </a>
+            </li>
+            <li class="">
+                <a href="/admin/dashboard" class="">
+                    <div class="parent-icon p-1"><i class="bi bi-list-columns me-0 h5"></i></div>
+                </a>
+            </li>
+            <li class="">
+                <a href="/admin/dashboard" class="">
+                    <div class="parent-icon p-1"><i class="bi bi-list-columns me-0 h5"></i></div>
+                </a>
+            </li>
+
+        </ul>
+    </div>
 </div>
 
 @push('scripts')
     <script>
         const sidebarButtons = $('a#sidebarButton');
-        sidebarButtons.each(function(){
-            $(this).on('mouseenter', function(){
+        sidebarButtons.each(function() {
+            $(this).on('mouseenter', function() {
                 $(this).addClass('btn-primary text-white');
-            }).on('mouseleave', function(){
+            }).on('mouseleave', function() {
                 $(this).removeClass('btn-primary text-white');
             });
         });
 
 
-        $().ready(function () {
+        $().ready(function() {
             $('#menu').metisMenu();
         });
 
 
 
-
+        let isToggledSidebar;
+        const toggleSidebar = () => {
+            if (isToggledSidebar == true) {
+                isToggledSidebar = false;
+                $('#sidebar').addClass('d-none');
+                $('#sidebarIconOnly').removeClass('d-none');
+            } else {
+                isToggledSidebar = true;
+                $('#sidebarIconOnly').addClass('d-none');
+                $('#sidebar').removeClass('d-none');
+            }
+        }
     </script>
 @endpush
